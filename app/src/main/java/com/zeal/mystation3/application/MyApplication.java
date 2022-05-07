f1d07436277c096774d6ba9b9af39dd7134eaa51
zealrussell0 <1057021029@qq.com>
2022-05-07 22:57:09 +0800

    uvc camera done

---
+++ app/src/main/java/com/zeal/mystation3/application/MyApplication.java
@@ -0,0 +1,24 @@
+package com.zeal.mystation3.application;
+
+import android.app.Application;
+
+import com.zeal.mystation3.utils.CrashHandler;
+
+
+/**application class
+ *
+ * Created by jianddongguo on 2017/7/20.
+ */
+
+public class MyApplication extends Application {
+    private CrashHandler mCrashHandler;
+    // File Directory in sd card
+    public static final String DIRECTORY_NAME = "USBCamera";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mCrashHandler = CrashHandler.getInstance();
+        mCrashHandler.init(getApplicationContext(), getClass());
+    }
+}
